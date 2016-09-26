db9df32b3fe6b768117df9488f842fa040adc41e
funky <mafunk92@gmail.com>
2016-09-25 22:02:17 -0500

    added react router, started adding testing files

--- app/components/app.js
+++ app/components/app.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
     render() {
         return (
-            <div>React simple starter</div>
+            <div>
+                React simple starter
+                <br />
+                <Link to="/">Home</Link>
+                <br />
+                <Link to="/one">One</Link>
+                { this.props.children }
+            </div>
     );
     }
 }
--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
-import App from './components/app';
+import routes from './routes';
 import reducers from './reducers';
 
 import styles from './styles/style.css'
@@ -12,7 +13,7 @@ import styles from './styles/style.css'
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <App />
+    <Provider store={ createStoreWithMiddleware(reducers) }>
+        <Router history={ hashHistory } routes={ routes } />
     </Provider>
     , document.getElementById('app'));
